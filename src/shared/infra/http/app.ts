913a8247fd3c8d1e6edd65171c341ee9a88ece66
unknown <diogosilvaoficial@gmail.com>
2022-12-03 23:16:36 -0300

    upload avatar e cars

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -5,6 +5,8 @@ import express, { NextFunction, Request, Response } from "express";
 import "express-async-errors";
 import swaggerUi from "swagger-ui-express";
 
+import upload from "@config/upload";
+import { AppError } from "@shared/errors/AppError";
 import createConnection from "@shared/infra/typeorm";
 
 import swaggerFile from "../../../swagger.json";
@@ -12,14 +14,15 @@ import swaggerFile from "../../../swagger.json";
 import "@shared/container";
 import { router } from "./routes";
 
-import { AppError } from "@shared/errors/AppError";
-
 createConnection();
 const app = express();
 
 app.use(express.json());
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
+
+app.use("/avatar", express.static(`${upload.tmpFolder}/avatar`));
+app.use("/cars", express.static(`${upload.tmpFolder}/cars`));
 
 app.use(router);
 
--- src/shared/infra/http/routes/users.routes.ts
+++ src/shared/infra/http/routes/users.routes.ts
@@ -1,9 +1,11 @@
-import { ensureAuthenticated } from "@shared/infra/http/middlewares/ensureAuthenticated";
-import { CreateUserController } from "@modules/accounts/useCases/createUser/CreateUserController";
-import { UpdateUserAvatarController } from "@modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController";
 import { Router } from "express";
 import multer from "multer";
+
 import uploadConfig from "@config/upload";
+import { CreateUserController } from "@modules/accounts/useCases/createUser/CreateUserController";
+import { ProfileUserController } from "@modules/accounts/useCases/profileUserUseCase/ProfileUserController";
+import { UpdateUserAvatarController } from "@modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController";
+import { ensureAuthenticated } from "@shared/infra/http/middlewares/ensureAuthenticated";
 
 const usersRoutes = Router();
 
@@ -11,6 +13,7 @@ const uploadAvatar = multer(uploadConfig);
 
 const createUserController = new CreateUserController();
 const updateUserAvatarController = new UpdateUserAvatarController();
+const profileUserController = new ProfileUserController();
 
 usersRoutes.post("/", createUserController.handle);
 
@@ -21,5 +24,6 @@ usersRoutes.patch(
   updateUserAvatarController.handle
 );
 
-export { usersRoutes };
+usersRoutes.get("/profile", ensureAuthenticated, profileUserController.handle);
 
+export { usersRoutes };
